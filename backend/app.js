675e6070547da302bc8d2ddd62ee691ca3aaa80d
sinharohan2000 <sinharohan2000@gmail.com>
2022-06-13 16:40:44 +0530

    Order API created

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,17 @@ const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 app.use(cookieParser());
-//Route imports
 
+
+//Route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //Middleware for errors
 app.use(errorMiddleware);
