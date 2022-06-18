01ce82c317275a9e39ed3a74ce3ccbc75770e995
rishav rauniyar <rishavrauniyar88@gmail.com>
2022-06-19 02:10:06 +0545

    login registration component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails";
 import Products from "./component/Product/Products.js";
 import Search from "./component/Product/Search";
+import LoginSignUp from "./component/User/LoginSignUp";
 
 function App() {
   React.useEffect(() => {
@@ -28,6 +29,8 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/products/:keyword" element={<Products />} />
         <Route path="/search" element={<Search />} />
+
+        <Route path="/login" element={<LoginSignUp/>} />
       </Routes>
 
       {/* <Home/> */}
